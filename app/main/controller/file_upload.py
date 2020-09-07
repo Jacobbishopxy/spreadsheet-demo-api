8a9125ad7d220e4d1d64026edcb7111df8427588
Jacob <jacobbishopxy@gmail.com>
2020-09-07 18:49:00 +0800

    MNT: update file_upload.py

--- app/main/controller/file_upload.py
+++ app/main/controller/file_upload.py
@@ -9,13 +9,17 @@ import pandas as pd
 
 ns = Namespace("upload", description="Upload Excel and return spreadsheet")
 
-
 xlsx_file = "xlsx_file"
 xlsx_file_type = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
 parser = reqparse.RequestParser()
 parser.add_argument(xlsx_file,
                     type=FileStorage,
                     location="files")
+param_head, param_multi_sheets = "head", "multiSheets"
+parser.add_argument(param_head,
+                    type=str)
+parser.add_argument(param_multi_sheets,
+                    type=str)
 
 
 @ns.route("/upload")
@@ -25,9 +29,15 @@ class FileUploadAPI(Resource):
     def post(self):
         args = parser.parse_args()
         f = args.get(xlsx_file)
+
+        hd = 0 if args.get(param_head) == "true" else None
+        ms = None if args.get(param_multi_sheets) == "true" else 0
         if f is not None and f.content_type == xlsx_file_type:
-            foo = pd.read_excel(f)
-            print(foo)
+            xs = pd.read_excel(f, header=hd, sheet_name=ms)
+            if isinstance(xs, dict):
+                res = {k: v.fillna("").to_dict(orient="records") for k, v in xs.items()}
+            else:
+                res = xs.fillna("").to_dict(orient="records")
+            return res
         else:
             abort(404)
-        return {'status': 'Done'}
--- app/main/controller/hello.py
+++ app/main/controller/hello.py
@@ -3,13 +3,22 @@
 @time 9/3/2020
 """
 
-from flask_restx import Resource, Namespace
+from flask_restx import Resource, Namespace, reqparse
 
 ns = Namespace("hello")
+
+param_name = "name"
+parser = reqparse.RequestParser()
+parser.add_argument(param_name, type=str)
 
 
 @ns.route("/hello")
 class HelloWorld(Resource):
 
+    @ns.expect(parser)
     def get(self):
+        args = parser.parse_args()
+        n = args.get(param_name)
+        if n is not None:
+            return f"Hello {n}!"
         return "Hello World!"
